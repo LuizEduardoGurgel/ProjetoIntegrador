e0caa1adcb24624727ac2af4d0455ae53dbf44ca
Luiz Eduardo Gurgel Marcelino de Oliveira <20221144010013@ifrn.local>
2024-11-29 14:31:24 -0300

    Descrição das mudanças realizadas

---
+++ js/script.js
@@ -0,0 +1,10 @@
+function mostrarGitHub() {
+    var githubLinks = `
+        GitHub 1: https://github.com/LuizEduardoGurgel
+        GitHub 2: https://github.com/carneiroaraujo
+        GitHub 3: https://github.com/Hermesdanubio
+        GitHub 4: https://github.com/paulvitor
+        GitHub 5: https://github.com/
+    `;
+    window.alert(githubLinks);
+}
